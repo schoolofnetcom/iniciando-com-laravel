0fee0b4b7ba78d3b882aa4610695e2c17802328c
Thiago Valls <thiago@trinityweb.com.br>
2017-10-04 04:40:37 -0300

    04_routes

---
+++ 04_rotas/highway2laravel/app/Http/routes.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('foo', function () {
+        return "Este método é GET";
+    });
+
+    Route::post('foo', function () {
+        return "Este método é POST";
+    });
+
+    Route::group(['prefix' => 'user'], function(){
+        Route::get('/', function () {
+            return "Listagem de usuários";
+        });
+
+        Route::get('add', function () {
+            return "Adicionar usuário";
+        });
+
+        Route::get('{id}', function ($id) {
+            return "Este é o usuário {$id}";
+        });
+    });
+});
