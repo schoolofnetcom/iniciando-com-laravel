ba214d60cd8bc71495ba7292c0957d5e059ecbf8
Thiago Valls <thiago@trinityweb.com.br>
2017-10-05 02:40:36 -0300

    08_migrations

---
+++ 08_migrations/highway2laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = ['Wesley', 'Matheus', 'Guilherme'];
+
+        return view('user.index', compact('users'));
+    }
+
+    public function show($id)
+    {
+        return view('user.show', compact('id'));
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function post(Request $request)
+    {
+        $this->validate($request, [
+           'name' => ['required'],
+           'email' => ['required','email'],
+           'password' => ['required','confirmed', 'min:4'],
+        ]);
+    }
+}
---
+++ 08_migrations/highway2laravel/app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::group(['prefix' => 'user'], function(){
+        Route::get('/', ['uses' => 'UserController@index']);
+        Route::get('/add', ['uses' => 'UserController@create']);
+        Route::post('/add', ['uses' => 'UserController@post']);
+        Route::get('{id}', ['uses' => 'UserController@show']);
+    });
+});
